f03ce5c03cd41fed683ca75253b65d183efaba17
Pamela Peixinho <pameiupip@gmail.com>
2016-02-11 14:11:10 -0200

    Initial Commit

---
+++ collectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collectionView
+//
+//  Created by Pamela Iupi Peixinho on 2/11/16.
+//  Copyright © 2016 PamelaPeixinho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ collectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  collectionView
+//
+//  Created by Pamela Iupi Peixinho on 2/11/16.
+//  Copyright © 2016 PamelaPeixinho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
